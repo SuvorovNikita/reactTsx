8fc683e4d66402e6b70db9bf7b3569db31aadbfd
SuvorovNikita <nikit.suvorv@gmail.com>
2020-05-13 16:28:35 +0300

    Updates

---
+++ precache-manifest.fb76234ae974286dcc5d68d5f5a59ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f986896f4786c873ac3e5619c31cb0",
+    "url": "/reactTsx/index.html"
+  },
+  {
+    "revision": "14c873c75e228e208ef9",
+    "url": "/reactTsx/static/css/main.a319c60e.chunk.css"
+  },
+  {
+    "revision": "0ff8aef84d9a804b65b6",
+    "url": "/reactTsx/static/js/2.ef82f7fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTsx/static/js/2.ef82f7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c873c75e228e208ef9",
+    "url": "/reactTsx/static/js/main.744e8239.chunk.js"
+  },
+  {
+    "revision": "d2defa3e0d1c50c0ec0e",
+    "url": "/reactTsx/static/js/runtime-main.4ec33f78.js"
+  }
+]);
