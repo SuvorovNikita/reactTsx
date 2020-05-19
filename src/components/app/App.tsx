fcd1612b965ed46f5c84c8384ed2245fbcd2e911
SuvorovNikita <nikit.suvorv@gmail.com>
2020-05-19 09:57:02 +0300

    add components ui

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
         {id: v1(), texts: 'качество 1', active: false},
         {id: v1(), texts: 'качество 2', active: true},
         {id: v1(), texts: 'качество 3', active: false}
+        {id: v1(), texts: 'качество 3', active: false}
     ]);
 
     let [names, setNames] = useState([
