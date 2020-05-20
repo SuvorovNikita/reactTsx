a3e7252f0912820a9a29e918ce38adcf4ed79868
SuvorovNikita <nikit.suvorv@gmail.com>
2020-05-20 14:16:21 +0300

    refactoring

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,5 +1,6 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import style from './counter.module.css';
+
 
 type NamesType = {
     id: string
@@ -15,23 +16,40 @@ const Counter = (props: PropsType) => {
 
     let [counts, setCounts] = useState(0);
     let [name, setName] = useState('');
+    let [error, setError] = useState();
+
     let {addName} = props;
+    let styleError = error ? style.error : '';
 
     function upName(name: string) {
-        setCounts(counts + 1);
-        addName(name);
-        alert('Привет' + ' ' + (name));
-        setName('');
+        let trimName = name.trim();
+        if (trimName) {
+            setCounts(counts + 1);
+            addName(name);
+            alert('Привет' + ' ' + (name));
+            setName('');
+        } else setError(style);
     }
 
     function onChangeText(e: ChangeEvent<HTMLInputElement>) {
         setName(e.currentTarget.value)
     }
 
+    function onKeyEnter(e: KeyboardEvent<HTMLInputElement>) {
+        setError('');
+        if (e.charCode === 13) {
+            upName(name);
+        }
+    }
+
+    const onClickName = () => {
+        upName(name)
+    };
+
     return (<div className={style.form}>
             <span>{counts}</span>
-            <input type='text' value={name} onChange={onChangeText}/>
-            <button onClick={()=>{upName(name)}}>Click</button>
+            <input type='text' value={name} className={styleError} onKeyPress={onKeyEnter} onChange={onChangeText}/>
+            <button onClick={onClickName}>Click</button>
         </div>
     )
 };
--- src/components/qualities/qualitie/qualitie.tsx
+++ src/components/qualities/qualitie/qualitie.tsx
@@ -2,22 +2,12 @@ import React from "react";
 import styles from "./qualitie.module.css";
 
 
-type TextType = {
-    texts: string
-    id: string
-    active: boolean
-}
-
-type PropsType ={
-    texts: Array<TextType>
-}
-
 
 const Qualitie = (props: any) => {
     let {t} = props;
     let activeClass = t.active ? styles.textActive : styles.text;
     return (
-        <div className={activeClass}>{t.texts}</div>
+        <p className={activeClass}>{t.texts}</p>
     )
 };
 
